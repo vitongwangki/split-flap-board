01e2d487e2c48ff187a1f77b840d141cce37cdef
vitongwangki <vito.ngwangki@gmail.com>
2024-01-13 12:53:40 +0400

    Flip function both upper and lower

--- module-animation.js
+++ module-animation.js
@@ -1,5 +1,34 @@
-let upper = document.getElementbyId('letterUpperHalf');
-let lower = document.getElementbyId('letterLowerHalf');
+let targetChar = 'H';
 
-upper.innerHTML = 'A';
-lower.innerHTML = 'A';
+
+const flip = (target) => {
+    let upper = document.getElementById('letterUpperHalf').innerHTML;
+    let lower = document.getElementById('letterLowerHalf').innerHTML;
+    console.log(upper.charCodeAt(0));
+    console.log(lower.charCodeAt(0));
+    console.log(target.charCodeAt(0));
+    while (upper !== target) {
+        if (upper.charCodeAt(0) > target.charCodeAt(0)) {
+            upper = String.fromCharCode(upper.charCodeAt(0) - 1);
+            document.getElementById('letterUpperHalf').innerHTML = upper;
+            console.log(upper);
+        } else if (upper.charCodeAt(0) < target.charCodeAt(0)) {
+            upper = String.fromCharCode(upper.charCodeAt(0) + 1);
+            document.getElementById('letterUpperHalf').innerHTML = upper;
+            console.log(upper);
+        };
+    };
+    while (lower !== target) {
+        if (lower.charCodeAt(0) > target.charCodeAt(0)) {
+            lower = String.fromCharCode(lower.charCodeAt(0) - 1);
+            document.getElementById('letterLowerHalf').innerHTML = lower;
+            console.log(lower);
+        } else if (lower.charCodeAt(0) < target.charCodeAt(0)) {
+            upper = String.fromCharCode(lower.charCodeAt(0) + 1);
+            document.getElementById('letterLowerHalf').innerHTML = lower;
+            console.log(lower);
+        };
+    };
+}
+
+flip(targetChar);
